3313fb9f724bec3f5d9480e223cbe830e87fd027
L528113381 <528113381@qq.com>
2023-12-11 14:50:12 +0800

    wrap toast

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -1,4 +1,11 @@
 // app.js
+//方法一
+// import './utils/index'
+// wx.utils.toast()
+
+import utils from './utils/index'
+utils.toast()
+
 App({
   globalData: {},
 })
---
+++ miniprogram/utils/index.js
@@ -0,0 +1,15 @@
+const utils = {
+  toast(title = "数据加载失败..."){
+    wx.showToast({
+      title,
+      icon:'none',
+      duration:2000,
+      mask:true
+    })
+  }
+}
+// 将 'utils' 对象挂载到 'wx' 对象上
+wx.utils = utils
+//es5导出
+export default utils
+
